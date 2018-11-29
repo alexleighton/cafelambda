eaafad019b13c77e0baf9680dafbcce76727c1eb
Alex Leighton <axlelonghorn@gmail.com>
2018-11-28 21:54:25 -0800

    Initial commit.

---
+++ src/main/java/alonzo/Main.java
@@ -0,0 +1,9 @@
+package alonzo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("success");
+    }
+
+}
---
+++ src/test/java/alonzo/FirstTest.java
@@ -0,0 +1,15 @@
+package alonzo;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FirstTest {
+
+    @Test
+    public void success() {
+        assertThat(true, equalTo(true));
+    }
+
+}
